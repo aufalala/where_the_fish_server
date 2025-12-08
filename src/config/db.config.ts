b261a4f8514a9ce165d5c08d90d6b2b7b3d87a97
Aufa <aufa_romeo@live.com>
2025-12-09 02:47:04 +0800

    FEAT: mongodb connection setup

---
+++ src/config/db.config.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+
+import { MONGODB_URI } from "./env.config.js";
+
+import { getTimestamp } from "../utils/timestamp.js";
+
+mongoose.connection.on("connected", () => console.log(`[${getTimestamp()}] Connected to MongoDB: ${mongoose.connection.name}`));
+mongoose.connection.on("error", (err) => console.error(`[${getTimestamp()}] MongoDB error:`, err));
+mongoose.connection.on("disconnected", () => console.warn(`[${getTimestamp()}] MongoDB disconnected`));
+
+export async function connectDB() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+  } catch (err) {
+    console.error(`[${getTimestamp()}] MongoDB connection error:`, err);
+  }
+}
