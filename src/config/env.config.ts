1fcde6def7eb604f8e7436c6cfd5d5e0aaed3efd
Aufa <aufa_romeo@live.com>
2025-12-09 02:46:40 +0800

    FEAT: env config

---
+++ src/config/env.config.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const requiredEnv = [
+  "PORT",
+  "MONGODB_URI",
+  "ACCESS_TOKEN_SECRET",
+  "REFRESH_TOKEN_SECRET",
+] as const;
+
+for (const key of requiredEnv) {
+  if (!process.env[key]) {
+    throw new Error(`Missing required environment variable: ${key}`);
+  }
+}
+
+export const PORT = process.env.PORT || 3000;
+export const MONGODB_URI = process.env.MONGODB_URI as string;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
