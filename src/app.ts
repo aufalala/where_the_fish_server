a82e1da085cb2537a884d68c0500b83732fd5dd9
Aufa <aufa_romeo@live.com>
2025-12-08 22:04:31 +0800

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+// import userRoutes from "./routes/user.routes.js";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// app.use("/api/users", userRoutes);
+
+export default app;
