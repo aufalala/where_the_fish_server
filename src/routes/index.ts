1c69a2f07f2088d74de66a963799cd75d516a709
Aufa <aufa_romeo@live.com>
2025-12-12 17:52:56 +0800

    FEAT: index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-// import userRoutes from "./user.routes.js";
+
 import authRoutes from "./auth.routes.js";
+import postRoutes from "./post.routes.js";
 
 const router = Router();
 
-// router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/posts", postRoutes);
 
 export default router;
